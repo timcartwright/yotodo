b1c2ddc621f2b7ad861e8387340dc86627015914
timcartwright <tim@websitesbytim.com>
2015-11-25 15:45:54 +0530

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the todoApp
+ */
+angular.module('todoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = [];
+
+    $scope.addTodo = function() {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+
+    $scope.removeTodo = function(index) {
+      $scope.todos.splice(index, 1);
+    };
+  });
