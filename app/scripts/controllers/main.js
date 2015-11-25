7c50f61f7fb1bd2ab2a7e7dcb65671951fb7c1e3
timcartwright <tim@websitesbytim.com>
2015-11-25 17:56:52 +0530

    Add Local Storage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,14 @@
  * Controller of the todoApp
  */
 angular.module('todoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+
+    var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
 
     $scope.addTodo = function() {
       $scope.todos.push($scope.todo);
